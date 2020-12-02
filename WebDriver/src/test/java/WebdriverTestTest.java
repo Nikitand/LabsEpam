08c7a5a01f816d5aae50e4125a62916db127568b
Nikitand <nikitos_by_brest@mail.ru>
2020-12-02 23:28:57 +0300

    for Jenkins

--- WebDriver/src/test/java/WebdriverTestTest.java
+++ WebDriver/src/test/java/WebdriverTestTest.java
@@ -4,22 +4,33 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class WebdriverTestTest {
     WebDriver driver;
     Actions moveToButton;
     WebDriverWait wait;
+    private static ChromeDriverService service;
+    static  int time = 25;
     @BeforeTest
-    void Start() {
+    void Start() throws IOException {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 25);
+        wait = new WebDriverWait(driver, time);
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("C:\\driver\\chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        service.start();
     }
     @Test
     public void searchWithParametrs()
@@ -46,7 +57,7 @@ public class WebdriverTestTest {
         WebElement moskowcityCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='content-container']//section//div[2]//form/div[7]/div/div[1]/div[2]")));
         moskowcityCheckbox.click();
 
-List<WebElement> searchResult = driver.findElements(By.id("search-result-items"));
+        List<WebElement> searchResult = driver.findElements(By.id("search-result-items"));
         Assert.assertTrue("search result are empty!", searchResult.size()>0);
 
 
@@ -54,6 +65,7 @@ List<WebElement> searchResult = driver.findElements(By.id("search-result-items")
     @AfterTest(alwaysRun = true)
     void Finish()
     {
+        service.stop();
         driver.quit();
     }
 }
