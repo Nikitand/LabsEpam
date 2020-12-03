f2ac263ee8a625bfdb03b3318130e24ba53f4c96
Nikitand <nikitos_by_brest@mail.ru>
2020-12-03 16:05:02 +0300

    last

--- WebDriver/src/test/java/WebDriverTest.java
+++ WebDriver/src/test/java/WebDriverTest.java
@@ -10,57 +10,50 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-
 import java.util.List;
 
 public class WebDriverTest {
 
     private WebDriver driver;
-    private WebDriverWait wait;
 
-    static   int TIME_OUT_SECONDS = 25;
+    private static final int TIME_OUT_SECONDS = 25;
 
     @BeforeTest
-    void start()  {
+  public   void start()  {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, TIME_OUT_SECONDS);
-            }
+    }
+
     @Test
-    public void searchWithParametrs()
-    {
+    public void searchWithParametrs()  {
         driver.get("https://lacoste.ru/");
         driver.manage().window().maximize();
 
-        WebElement searchInput = driver.findElement(By.xpath("//*[@id='search-render']/form/input[1]"));
+        WebElement searchInput = driver.findElement(By.xpath("//input[@class='q' and @itemprop='query-input']\n"));
         searchInput.sendKeys("КУРТКА");
         searchInput.sendKeys(Keys.ENTER);
 
-        WebElement genderCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Пол')]")));
-        genderCheckbox.click();
-        WebElement menCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='content-container']//div[2]//div[2]//div[3]")));
-        menCheckbox.click();
-        WebElement sessionCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Сезон')]")));
-        sessionCheckbox.click();
-        WebElement winterCheckBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='content-container']//div[2]//form/div[2]//div[2]")));
-        winterCheckBox.click();
-        WebElement sizeCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Цвет')]")));
-        sizeCheckbox.click();
-        WebElement oneSizeCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='content-container']//div[2]//div[6]//div[2]")));
-        oneSizeCheckbox.click();
-        WebElement cityCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Наличие')]")));
-        cityCheckbox.click();
-        WebElement moskowcityCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[contains(text(),'Вегас, Москва')]")));
-        moskowcityCheckbox.click();
+        clickButtonByXpath(By.xpath("//button[contains(text(),'Пол')]"));
+        clickButtonByXpath(By.xpath("//label[contains(text(),'Муж')]"));
+        clickButtonByXpath(By.xpath("//button[contains(text(),'Сезон')]"));
+        clickButtonByXpath(By.xpath("//label[contains(text(), \"зима\") and @class =\"filterbtn\"]"));
+        clickButtonByXpath(By.xpath("//button[contains(text(),'Размер') and @ class=\"extern-fit nav-ico-after\"]"));
+        clickButtonByXpath(By.xpath("//label[contains(text(),'46')]"));
+        clickButtonByXpath(By.xpath("//button[contains(text(),'Наличие')]"));
+        clickButtonByXpath(By.xpath("//label[contains(text(),'Вегас, Москва')]"));
+
+
 
         List<WebElement> searchResult = driver.findElements(By.id("search-result-items"));
         Assert.assertTrue("search result are empty!", searchResult.size()>0);
-
-
     }
-    @AfterTest(alwaysRun = true)
-    void finish()
-    {
 
+    @AfterTest(alwaysRun = true)
+    public void finish()   {
         driver.quit();
+    }
+    private void clickButtonByXpath(By by) {
+        new WebDriverWait(driver, TIME_OUT_SECONDS)
+                .until(ExpectedConditions.presenceOfElementLocated(by))
+                .click();
     }
 }
